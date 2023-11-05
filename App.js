41cbbbd96207ad31ba6ceb0616449c66c0d0a68c
effany <72710849+effany@users.noreply.github.com>
2023-11-05 09:55:56 +0100

    all the minigame app code

--- App.js
+++ App.js
@@ -1,20 +1,80 @@
+import { StyleSheet, ImageBackground, SafeAreaView} from 'react-native';
+import {useState} from "react"
+import { LinearGradient } from 'expo-linear-gradient'
+import Colors from './constants/color';
+import StartGameScreen from './screens/StartGameScreen';
+import GameScreen from './screens/GameScreen';
+import GameOverScreen from './screens/GameOverScreen';
+import { useFonts } from 'expo-font';
+import AppLoading from 'expo-app-loading';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
 
 export default function App() {
+  const [userNumber, setUserNumber] = useState();
+  const [gameIsOver, setGameIsOver] = useState(true)
+  const [guessRounds, setGuessRounds] = useState(0)
+
+  const [fonsLoaded] = useFonts({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
+  })
+
+  if (!fonsLoaded) {
+    return <AppLoading />
+  }
+
+  function pickedNumberHandler(pickedNumber) {
+    setUserNumber(pickedNumber)
+    setGameIsOver(false)
+  }
+
+  function gameOverHandler(numberOfRounds) {
+    setGameIsOver(true); 
+    setGuessRounds(numberOfRounds)
+  }
+
+  function startNewGameHandler() {
+    setUserNumber(null);
+    setGuessRounds(0);
+  }
+
+  let screen = <StartGameScreen onPickNumber={pickedNumberHandler}/>;
+
+  if (userNumber) {
+    screen = <GameScreen userNumber={userNumber} onGameOver={gameOverHandler}/>
+  }
+
+  if (gameIsOver && userNumber) {
+    screen = <GameOverScreen userNumber={userNumber} roundsNumber={guessRounds} onStartNewGame={startNewGameHandler}/>
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar style='light'></StatusBar>
+      <LinearGradient colors={[Colors.primary700, Colors.accent500 ]} style={styles.rootScreen}>
+        <ImageBackground 
+          source={require('./assets/images/background.png')} 
+          resizeMode='cover'
+          style={styles.rootScreen}
+          imageStyle={styles.backgrounImage}
+          >
+          
+          <SafeAreaView style={styles.rootScreen}>
+          {screen}
+          </SafeAreaView>
+        </ImageBackground>
+      </LinearGradient>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  rootScreen: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  }, 
+  backgrounImage: {
+    opacity: 0.15
+  }
 });
+
